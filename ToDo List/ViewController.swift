e40401ebc6249ab2605bbe3e9e3398ee2cfdde16
Magaly Trevino <magaly.trevino@utrgv.edu>
2023-07-15 15:04:03 -0500

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Magaly Trevino on 7/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
