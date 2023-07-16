1475e72e2338b86e0fa379ac192a4e314d96f4a9
Magaly Trevino <magaly.trevino@utrgv.edu>
2023-07-16 15:36:04 -0500

    Completed 5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Magaly Trevino on 7/16/23.
+//
+
+import Foundation
+
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
