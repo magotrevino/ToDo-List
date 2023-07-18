e0cacd1059b904b814e2807a5a83859419ce19e2
Magaly Trevino <magaly.trevino@utrgv.edu>
2023-07-17 19:27:40 -0500

    Completed 5.13

--- ToDo List/ListTableViewCell.swift
+++ ToDo List/ListTableViewCell.swift
@@ -12,12 +12,19 @@ protocol ListTableViewCellDelegate: AnyObject {
 }
 
 class ListTableViewCell: UITableViewCell {
-    
-    weak var delegate: ListTableViewCellDelegate?
-    
 
     @IBOutlet weak var checkBoxButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
+    
+    weak var delegate: ListTableViewCellDelegate?
+    
+    var toDoItem: ToDoItem! {
+        didSet {
+            nameLabel.text = toDoItem.name
+            checkBoxButton.isSelected = toDoItem.completed
+        }
+    }
+    
     @IBAction func checkToggle(_ sender: UIButton) {
         delegate?.checkBoxToggel(sender: self)
     }
--- ToDo List/ToDoDEtailTableViewController.swift
+++ ToDo List/ToDoDEtailTableViewController.swift
@@ -33,8 +33,16 @@ class ToDoDEtailTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //hide keyboard if we tap outside of a field
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
+        nameField.delegate = self
+        
         if toDoItem == nil {
             toDoItem = ToDoItem(name: "", date: Date().addingTimeInterval(24*60*60), notes: "", reminderSet: false, completed: false)
+            nameField.becomeFirstResponder()
         }
         updateUserInterface()
 
@@ -47,11 +55,20 @@ class ToDoDEtailTableViewController: UITableViewController {
         reminderSwitch.isOn = toDoItem.reminderSet
         dateLabel.textColor = (reminderSwitch.isOn ? .black : .gray)
         dateLabel.text = dateFormatter.string(from: toDoItem.date)
+        enableDisableSaveBurron(text: nameField.text!)
         
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text, reminderSet: reminderSwitch.isOn, completed: toDoItem.completed)
+    }
+    
+    func enableDisableSaveBurron(text: String) {
+        if text.count > 0 {
+            saveBarButton.isEnabled = true
+        } else {
+            saveBarButton.isEnabled = false
+        }
     }
     
     
@@ -67,14 +84,20 @@ class ToDoDEtailTableViewController: UITableViewController {
     }
     
     @IBAction func reminderSwitchChanged(_ sender: UISwitch) {
+        self.view.endEditing(true)
         dateLabel.textColor = (reminderSwitch.isOn ? .black : .gray)
         tableView.beginUpdates()
         tableView.endUpdates()
-
     }
     
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        self.view.endEditing(true)
         dateLabel.text = dateFormatter.string(from: sender.date)
+    }
+    
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        enableDisableSaveBurron(text: sender.text!)
+
     }
     
 }
@@ -89,5 +112,12 @@ extension ToDoDEtailTableViewController {
         default:
             return defaultRowHeight
         }
+    }
+}
+
+extension ToDoDEtailTableViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        noteView.becomeFirstResponder()
+        return true
     }
 }
